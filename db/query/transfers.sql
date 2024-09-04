933cf4447bcb85d1879de444236c67090911061f
Ritwiksrivastava0809 <srivastavaritwik877@gmail.com>
2024-09-04 11:56:26 +0530

    feat: added sql functionality for entry and transfer table

---
+++ db/query/transfers.sql
@@ -0,0 +1,24 @@
+-- name: InsertTransaction :one
+INSERT INTO transfers(
+    from_account_id,
+    to_account_id,
+    amount
+)VALUES(
+    $1 , $2 , $3
+)RETURNING *;
+
+-- name: GetTransactionByID :one
+SELECT * FROM transfers 
+WHERE id = $1 LIMIT 1;
+
+-- name: GetTransactionHistoryByAccountID :many
+SELECT * FROM transfers
+WHERE from_account_id = $1 
+ORDER BY id
+LIMIT $2
+OFFSET $3;
+
+-- name: DeleteTransactionByID :exec
+DELETE FROM 
+transfers WHERE 
+id = $1;
