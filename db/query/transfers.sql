41f96dce2700f34f156924bc566066a84230e3b2
Ritwiksrivastava0809 <srivastavaritwik877@gmail.com>
2024-09-04 12:20:29 +0530

    fix: added listtransfer and listentries query

--- db/query/transfers.sql
+++ db/query/transfers.sql
@@ -22,3 +22,12 @@ OFFSET $3;
 DELETE FROM 
 transfers WHERE 
 id = $1;
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+WHERE 
+    from_account_id = $1 OR
+    to_account_id = $2
+ORDER BY id
+LIMIT $3
+OFFSET $4;
