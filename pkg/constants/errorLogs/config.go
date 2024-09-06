a7f12b3a3f101f8662ebb116dbd30a186cd086b6
Ritwiksrivastava0809 <srivastavaritwik877@gmail.com>
2024-09-07 01:42:08 +0530

    fix: corected error logs

--- pkg/constants/errorLogs/config.go
+++ pkg/constants/errorLogs/config.go
@@ -2,7 +2,7 @@ package errorLogs
 
 const (
 	ParsingError          = "got error %v on parsing configuration file"
-	BindingJsonError      = "Error while binding the request: %s"
-	GetCreateAccountError = "Error while creating the account: %s"
-	GetAccountError       = "Error while getting the account: %s"
+	BindingJsonError      = "got error while binding the request: %s"
+	GetCreateAccountError = "got error while creating the account: %s"
+	GetAccountError       = "got error while getting the account: %s"
 )
