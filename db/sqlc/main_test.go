4114af5f91d6f70ae05ff09fe251bd59ccde1dba
Ritwiksrivastava0809 <srivastavaritwik877@gmail.com>
2024-09-06 11:17:59 +0530

    feat: added transfer tx and written unit test for it

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -10,21 +10,25 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+// DB connection details
 const (
 	DBDriver = "postgres"
 	DBSource = "postgresql://root:password@localhost:5432/bank?sslmode=disable"
 )
 
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(DBDriver, DBSource)
+	var err error
+
+	testDB, err = sql.Open(DBDriver, DBSource)
 	if err != nil {
 		message := fmt.Sprintf("got error getting db %v", err)
 		log.Error().Msg("Got error while getting db in test main error :: " + message)
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	os.Exit(m.Run())
 }
