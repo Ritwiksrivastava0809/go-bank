25360241fc12567252e41c60a87666bcdf86b89e
Ritwiksrivastava0809 <srivastavaritwik877@gmail.com>
2024-09-10 23:43:06 +0530

    fix : removed unique username validation from user struct.

--- pkg/users/models.go
+++ pkg/users/models.go
@@ -1,7 +1,7 @@
 package users
 
 type User struct {
-	UserName string `json:"username" binding:"required" unique:"true"`
+	UserName string `json:"username" binding:"required"`
 	Email    string `json:"email" binding:"required"`
 	FullName string `json:"full_name" binding:"required"`
 	Password string `json:"password" binding:"required" validate:"min=8,max=20"`
