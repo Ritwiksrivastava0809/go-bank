bdf061f9dead0a4c641225931358190e66fe5dee
Ritwik srivastava <112408652+Ritwiksrivastava0809@users.noreply.github.com>
2024-09-14 23:44:01 +0530

    feat: added paseto token and unit tests (#26)
    * feat: added paseto token and unit tests
    
    * Update pkg/token/payload.go
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * fix : fixed naming convention
    
    ---------
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ pkg/token/paseto_maker.go
@@ -0,0 +1,52 @@
+package token
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/Ritwiksrivastava0809/go-bank/pkg/constants"
+	"github.com/Ritwiksrivastava0809/go-bank/pkg/constants/errorLogs"
+	"github.com/aead/chacha20poly1305"
+	"github.com/o1egl/paseto"
+)
+
+// NewPasstoMaker creates a new PasetoMaker
+func NewPasetoMaker(symetricKey string) (Maker, error) {
+
+	if len(symetricKey) != chacha20poly1305.KeySize {
+		return nil, fmt.Errorf(errorLogs.InvalidKeySize, chacha20poly1305.KeySize)
+	}
+
+	maker := &PasetoMaker{
+		symmetricKey: []byte(symetricKey),
+		paseto:       paseto.NewV2(),
+	}
+
+	return maker, nil
+}
+
+// CreateToken creates a new token for a specific username and duration
+func (maker *PasetoMaker) CreateToken(username string, duration time.Duration) (string, error) {
+	payload, err := NewPayload(username, duration)
+	if err != nil {
+		return "", err
+	}
+	return maker.paseto.Encrypt(maker.symmetricKey, payload, nil)
+}
+
+// VerifyToken checks if the token is valid or not
+func (maker *PasetoMaker) VerifyToken(token string) (*Payload, error) {
+
+	payload := &Payload{}
+
+	err := maker.paseto.Decrypt(token, maker.symmetricKey, payload, nil)
+	if err != nil {
+		return nil, fmt.Errorf(constants.InvalidToken)
+	}
+
+	err = payload.Valid()
+	if err != nil {
+		return nil, err
+	}
+	return payload, nil
+}
