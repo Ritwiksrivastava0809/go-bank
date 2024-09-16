d67e35a9e946a7f413d83142252b5b4187a3ba95
Ritwiksrivastava0809 <srivastavaritwik877@gmail.com>
2024-09-16 20:33:21 +0530

    fix : reverted changes taking acces token duration from config

--- pkg/utils/time.go
+++ pkg/utils/time.go
@@ -1,7 +1,23 @@
 package utils
 
-import "time"
+import (
+	"log"
+	"time"
+
+	"github.com/Ritwiksrivastava0809/go-bank/pkg/config"
+)
 
 func GetAccessTokenDuration() time.Duration {
-	return 15 * time.Minute // Ensure this matches your config
+	// Read the duration from a configuration file or environment variable
+	durationStr := config.GetAccessTokenDuration()
+	if durationStr == "" {
+		durationStr = "15m" // Default to 15 minutes if not configured
+	}
+	duration, err := time.ParseDuration(durationStr)
+	if err != nil {
+		// Handle the error, e.g., log it and return a default value
+		log.Printf("Failed to parse ACCESS_TOKEN_DURATION: %v", err)
+		return 15 * time.Minute
+	}
+	return duration
 }
